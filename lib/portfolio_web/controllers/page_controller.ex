fcd6b06ca39649a65e0f1fa2621b41d25b1a2d73
Andrew Todd <andrew.s.todd@wsu.edu>
2024-09-17 13:45:00 -0700

    Yes.

--- lib/portfolio_web/controllers/page_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule PortfolioWeb.PageController do
-  use PortfolioWeb, :controller
-
-  def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
-  end
-end
